8fac19dc9a2e124e6286b65200d18a167232beb4
Joakim Hellsén <tlovinator@gmail.com>
2024-12-26 05:40:22 +0100

    Refactor main.py to implement Browser class for improved structure and encapsulation

--- src/browser/main.py
+++ src/browser/main.py
@@ -3,27 +3,36 @@ from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
 
+class Browser(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.set_window_title()
+        self.resize_and_maximize_window()
+
+        central_widget = QWidget()
+        layout = QVBoxLayout(central_widget)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl("https://duckduckgo.com")
+
+        layout.addWidget(self.browser)
+        self.setCentralWidget(central_widget)
+
+    def set_window_title(self) -> None:
+        self.setWindowTitle("Blue Archive watching machine")
+
+    def resize_and_maximize_window(self) -> None:
+        screensize: QSize = self.screen().size() * self.screen().devicePixelRatio()
+        self.resize(int(screensize.width() * 0.8), int(screensize.height() * 0.8))
+        self.showMaximized()
+
+
 def main() -> None:
     app = QApplication([])
 
-    window = QMainWindow()
-    window.setWindowTitle("Blue Archive watching machine")
-
-    # Set window size to screen size
-    screensize: QSize = window.screen().size() * window.screen().devicePixelRatio()
-    window.resize(int(screensize.width() * 0.8), int(screensize.height() * 0.8))
-    window.showMaximized()
-
-    central_widget = QWidget()
-    layout = QVBoxLayout(central_widget)
-
-    browser = QWebEngineView()
-    browser.setUrl("https://duckduckgo.com")
-
-    layout.addWidget(browser)
-    window.setCentralWidget(central_widget)
-
+    window = Browser()
     window.show()
+
     app.exec()
 
 
