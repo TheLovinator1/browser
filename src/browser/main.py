f930997fe298fda0ddd3b460d16b2d126602044e
Joakim Hellsén <tlovinator@gmail.com>
2024-12-26 06:17:42 +0100

    Enhance Browser class: add type hints, improve window title documentation, and update window resizing logic

--- src/browser/main.py
+++ src/browser/main.py
@@ -1,10 +1,19 @@
-from PySide6.QtCore import QSize
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
+if TYPE_CHECKING:
+    from PySide6.QtCore import QSize
+
 
 class Browser(QMainWindow):
+    """Main window of the browser."""
+
     def __init__(self) -> None:
+        """Initialize the browser."""
         super().__init__()
         self.set_window_title()
         self.resize_and_maximize_window()
@@ -19,15 +28,22 @@ class Browser(QMainWindow):
         self.setCentralWidget(central_widget)
 
     def set_window_title(self) -> None:
-        self.setWindowTitle("Blue Archive watching machine")
+        """Set the title of the window to 'web browser'.
+
+        This is the default title of the window. When the user opens a new tab,
+        the title of the window will be updated to the title of the web page.
+        """
+        self.setWindowTitle("web browser")
 
     def resize_and_maximize_window(self) -> None:
+        """Resize the window to 80% of the screen size and maximize it."""
         screensize: QSize = self.screen().size() * self.screen().devicePixelRatio()
         self.resize(int(screensize.width() * 0.8), int(screensize.height() * 0.8))
         self.showMaximized()
 
 
 def main() -> None:
+    """Run the browser."""
     app = QApplication([])
 
     window = Browser()
